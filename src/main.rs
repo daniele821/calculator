bae20aa49c80d3c6c04e78de1c7f6d92a9554985
Daniele Muffato <danixgithub1@gmail.com>
2023-12-04 20:57:10 +0100

    added main function to solve expressions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("expression calculator!");
+use std::env;
+
+use calculator::expression::{
+    error::Error,
+    solver::{self, FixRules},
+};
+
+fn main() -> Result<(), Error> {
+    let args = env::args().skip(1).collect::<Vec<_>>().join(" ");
+    let mut tokens = solver::parse(&args, FixRules::DEF, &[])?;
+    let result = solver::solve(&mut tokens)?;
+    println!("Solution: {result}");
+    Ok(())
 }
