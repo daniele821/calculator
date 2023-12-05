e7053d725e895b19912c9a16c5b7e77049bfb830
Daniele Muffato <danixgithub1@gmail.com>
2023-12-05 18:41:49 +0100

    few additions to interactive shell

--- src/shell/mod.rs
+++ src/shell/mod.rs
@@ -1,3 +1,5 @@
+use std::process::Command;
+
 use rustyline::{error::ReadlineError, DefaultEditor};
 
 use crate::expression::solver;
@@ -5,12 +7,16 @@ use crate::expression::solver;
 pub fn run() {
     let mut rl = DefaultEditor::new().unwrap();
     loop {
-        let readline = rl.readline(">>> ");
+        let readline = rl.readline("\x1b[92m>>>\x1b[0m ");
         match readline {
             Ok(line) => {
                 rl.add_history_entry(line.as_str()).unwrap();
                 match &line[..] {
                     "exit" => break,
+                    "clear" => {
+                        Command::new("clear").spawn().unwrap().wait().unwrap();
+                    }
+                    "help" => println!("{}", help()),
                     _ => match solver::resolve(&line, &[], &[], true) {
                         Ok(res) => println!("\nSolution: {res}\n"),
                         Err(err) => println!("\n{err}\n"),
@@ -29,4 +35,16 @@ pub fn run() {
             }
         }
     }
+}
+
+fn help() -> String {
+    String::from(
+        "
+    Commands:
+    - exit      => close shell
+    - clear     => clear terminal
+    - help      => show this help message
+    - *         => parse as an expression
+",
+    )
 }
