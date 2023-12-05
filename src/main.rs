046241ecae441a87507558e34602caa5ddfb5cd8
Daniele Muffato <danixgithub1@gmail.com>
2023-12-05 13:30:35 +0100

    main immediatly prints to output (to avoid storing very long explanations)

--- src/main.rs
+++ src/main.rs
@@ -10,21 +10,20 @@ use calculator::{
 use fraction::Fraction;
 
 fn main() {
-    let mut expl = String::new();
-    let res = run(&mut expl);
-    println!("\nExplanation:\n{expl}");
+    let res = run();
     match res {
         Ok(res) => println!("Solution: {res}"),
         Err(err) => println!("{err}"),
     }
 }
 
-fn run(expl: &mut String) -> Result<Fraction, Error> {
+fn run() -> Result<Fraction, Error> {
+    println!("\nExplanation:");
     let args = env::args().skip(1).collect::<Vec<_>>().join(" ");
     let mut tokens = solver::parse(&args, &FixRules::all(), &CheckRules::all())?;
-    expl.push_str(&format!("{}\n", common::fmt(&tokens, None)));
+    println!("{}", common::fmt(&tokens, None));
     while solver::solve_one_op(&mut tokens)? {
-        expl.push_str(&format!("{}\n", common::fmt(&tokens, None)));
+        println!("{}\n", common::fmt(&tokens, None));
     }
     solver::get_result(&tokens)
 }
